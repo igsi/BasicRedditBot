49de430964d04449ce03dc57b5024bad9f095789
Obi-Wan Kenobi <igsi@users.noreply.github.com>
2017-03-24 07:54:07 +0200

    Added more tests for RedditWrapper

--- tests/test_redditWrapper.py
+++ tests/test_redditWrapper.py
@@ -12,51 +12,82 @@ class TestRedditWrapper(TestCase):
     def setUp(self):
         self.config = getConfiguration()
 
-        self.submission = type('DummySubmission', (object,), { "id": "some_id",
-                                                               "title": "some_title",
-                                                               "created_utc": 1234,
-                                                               "subreddit": "some_subreddit"})
+        self.submission = type('DummySubmission', (object,), { "id":            "some_id",
+                                                               "title":         "some_title",
+                                                               "created_utc":   1234,
+                                                               "subreddit":     "some_subreddit"})
 
-        self.submissionExpected = {"reddit_id": self.submission.id,
-                                   "type": "SUBMISSION",
-                                   "content": self.submission.title,
-                                   "timestamp": self.submission.created_utc,
-                                   "subreddit": str(self.submission.subreddit)}
+        self.submissionExpected = {RedditWrapper.id_field:          self.submission.id,
+                                   RedditWrapper.type_field:        "SUBMISSION",
+                                   RedditWrapper.content_field:     self.submission.title,
+                                   RedditWrapper.timestamp_field:   self.submission.created_utc,
+                                   RedditWrapper.subreddit_field:   str(self.submission.subreddit)}
 
-        self.comment = type('DummyComment', (object,), {"id": "some_id",
-                                                        "body": "some_content",
-                                                        "created_utc": 1234,
-                                                        "subreddit": "some_subreddit"})
+        self.comment = type('DummyComment', (object,), {"id":           "some_id",
+                                                        "body":         "some_content",
+                                                        "created_utc":  1234,
+                                                        "subreddit":    "some_subreddit"})
 
-        self.commentExpected = {"reddit_id": self.comment.id,
-                                "type": "COMMENT",
-                                "content": self.comment.body,
-                                "timestamp": self.comment.created_utc,
-                                "subreddit": str(self.comment.subreddit)}
+        self.commentExpected = {RedditWrapper.id_field:         self.comment.id,
+                                RedditWrapper.type_field:       "COMMENT",
+                                RedditWrapper.content_field:    self.comment.body,
+                                RedditWrapper.timestamp_field:  self.comment.created_utc,
+                                RedditWrapper.subreddit_field:  str(self.comment.subreddit)}
 
-    @mock.patch.object(RedditWrapper, "_itemsStream")
-    def test__itemsStream(self, mock_itemsStream):
-        """Mocks calls to method _itemStream of the RedditWrapper class.
-        Tests that it is called correctly when trying to retrieve comments/submissions."""
+
+    @mock.patch.object(RedditWrapper, "itemsStream")
+    def test_getSubmissionsStream(self, mock_itemsStream):
+        """Mock calls to method _itemStream.
+        Test that it is called correctly when trying to retrieve submissions."""
         reddit = RedditWrapper(self.config["reddit"], self.config["subreddits"])
 
         # Check that the submission stream is called correctly.
         reddit.getSubmissionsStream()
-        reddit._itemsStream.assert_called_with(reddit.subreddits.stream.submissions,
-                                               RedditWrapper._jsonifySubmission)
+        reddit.itemsStream.assert_called_with(reddit.subreddits.stream.submissions,
+                                               RedditWrapper.submissionToDict)
+
+    @mock.patch.object(RedditWrapper, "itemsStream")
+    def test_getCommentsStream(self, mock_itemsStream):
+        """Mock calls to method _itemStream.
+        Test that it is called correctly when trying to retrieve submissions."""
+        reddit = RedditWrapper(self.config["reddit"], self.config["subreddits"])
 
         # Check that the comments stream is called correctly.
         reddit.getCommentsStream()
-        reddit._itemsStream.assert_called_with(reddit.subreddits.stream.comments,
-                                               RedditWrapper._jsonifyComment)
+        reddit.itemsStream.assert_called_with(reddit.subreddits.stream.comments,
+                                               RedditWrapper.commentToDict)
 
-    def test_jsonifySubmission(self):
-        json_submission = RedditWrapper._jsonifySubmission(self.submission)
+    def test_itemsStream(self):
+        """ Test that the first 100 items retrieved by the items generator are discarded. """
+        items = []
+        for i in range(0, 100):
+            items.append(self.helper_createDbSubmission("TO_BE_DISCARDED", "", 0, ""))
+        items.append(self.helper_createDbSubmission("FIRST VALUE RETURNED", "", 0, ""))
+        items.append(self.helper_createDbSubmission("SECOND VALUE RETURNED", "", 0, ""))
 
-        self.assertDictEqual(json_submission, self.submissionExpected)
+        mock_itemsGenerator = mock.MagicMock()
+        mock_itemsGenerator.return_value = iter(items)
 
-    def test_jsonifyComment(self):
-        json_comment = RedditWrapper._jsonifyComment(self.comment)
+        generator = RedditWrapper.itemsStream(mock_itemsGenerator, RedditWrapper.submissionToDict)
 
-        self.assertDictEqual(json_comment, self.commentExpected)
+        returned_items = list(generator())
 
+        self.assertEqual(returned_items[0][RedditWrapper.id_field], "FIRST VALUE RETURNED")
+        self.assertEqual(returned_items[1][RedditWrapper.id_field], "SECOND VALUE RETURNED")
+
+    def test_submissionToDict(self):
+        submission = RedditWrapper.submissionToDict(self.submission)
+
+        self.assertDictEqual(submission, self.submissionExpected)
+
+    def test_commentToDict(self):
+        comment = RedditWrapper.commentToDict(self.comment)
+
+        self.assertDictEqual(comment, self.commentExpected)
+
+    @staticmethod
+    def helper_createDbSubmission(id, title, time, subreddit):
+        return type('DummySubmission', (object,), {"id":            id,
+                                                   "title":         title,
+                                                   "created_utc":   time,
+                                                   "subreddit":     subreddit})
